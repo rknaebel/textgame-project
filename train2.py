572ce8b1f381304987320978453342f1c63f0e10
Rene Knaebel <reneknaebel90@gmail.com>
2016-11-22 11:23:44 +0100

    changed train2 for testing

--- train2.py
+++ train2.py
@@ -7,8 +7,9 @@
 
 
 import numpy as np
+import random
 
-from model import NeuralQLearner
+from model import RNNQLearner
 from keras.preprocessing.text import text_to_word_sequence
 
 from replay_buffer import PrioritizedReplayBuffer
@@ -44,11 +45,12 @@ if __name__ == "__main__":
     num_objects = env.action_space.spaces[1].n
     vocab_size  = env.vocab_space
     seq_len     = 200
+    hist_size   = 10
 
-    model = NeuralQLearner( seq_len,vocab_size,
-                            args.embd_size,args.hidden1,args.hidden2,
-                            num_actions,num_objects,
-                            args.alpha,args.gamma,args.batch_size)
+    model = RNNQLearner(seq_len,vocab_size,args.embd_size,hist_size,
+                        args.hidden1,args.hidden2,
+                        num_actions,num_objects,
+                        args.alpha,args.gamma,args.batch_size)
 
     # Initialize replay memory
     replay_buffer = PrioritizedReplayBuffer(args.buffer_size, args.random_seed)
@@ -58,6 +60,7 @@ if __name__ == "__main__":
         ep_lens = []
         invalids = []
         quests_complete = []
+        deaths = []
         #
         # TRAIN Phase
         #
@@ -71,9 +74,9 @@ if __name__ == "__main__":
             s_text = env.reset()
             s = sent2seq(s_text, seq_len)
             #
-            for j in xrange(args.max_ep_steps):
+            for j in xrange(20):
                 # show textual input if so
-                if args.render: env.render()
+                #if args.render: env.render()
                 # choose action
                 if np.random.rand() <= epsilon:
                     a = env.action_space.sample()
@@ -89,33 +92,33 @@ if __name__ == "__main__":
                 # Keep adding experience to the memory until
                 # there are at least minibatch size samples
                 if  ((replay_buffer.size() > args.batch_size) and
-                    (j % args.rounds_per_learn == 0)):
+                    (j % 4 == 0)):
                     s_batch, a_batch, r_batch, t_batch, s2_batch = \
                         replay_buffer.sample_batch(args.batch_size)
                     # Update the networks each given the new target values
-                    l1, l2 = model.trainOnBatch(s_batch, a_batch, r_batch, t_batch, s2_batch)
+                    l, l1, l2 = model.trainOnBatch(s_batch, a_batch, r_batch, t_batch, s2_batch)
                     loss1 += l1; loss2 += l2
 
                 s = s2
                 ep_reward += r
                 cnt_invalid_actions += 1 if r == -0.1 else 0
 
-                if terminal:
-                    cnt_quest_complete += 1
-                    break
+                if terminal: break
 
             ep_lens.append(j+1)
             invalids.append(cnt_invalid_actions)
-            quests_complete.append(1 if terminal else 0)
+            quests_complete.append(1 if terminal and r>=1 else 0)
+            deaths.append(1 if terminal and r<=0 else 0)
             scores.append(ep_reward)
 
-            print("  Episode {:03d}/{:03d}/{:03d} | L(qsa) {:.4f} | L(qso) {:.4f} | len {:02d} | inval {:02d} | eps {:.4f} | r {: .2f} | {:02d}".format(
+            print("  Episode {:03d}/{:03d}/{:03d} | L(qsa) {:.4f} | L(qso) {:.4f} | len {:03d} | inval {:03d} | eps {:.4f} | r {: .2f} | {:1d} {:1d}".format(
                 epoch+1, episode+1, args.episodes_per_epoch, loss1, loss2, ep_lens[-1], invalids[-1], epsilon, scores[-1],
-                quests_complete[-1]))
-        print("> Training   {:03d} | len {:02.2f} | inval {:02.2f} | quests {:02.2f} | r {: .2f} ".format(
+                quests_complete[-1],deaths[-1]))
+        print("> Training   {:03d} | len {:02.2f} | inval {:02.2f} | quests {:02.2f} | deaths {:.2f} | r {: .2f} ".format(
             epoch+1, np.mean(ep_lens),
             np.mean(invalids),
             np.mean(quests_complete),
+            np.mean(deaths),
             np.mean(scores)))
 
         #
@@ -127,37 +130,42 @@ if __name__ == "__main__":
         quests_complete = []
         for episode in range(args.episodes_per_epoch):
             ep_reward = 0.
+            cnt_invalid_actions = 0
             # get initial input
             seed = random.random()
             env_eval.seed(seed)
-            s_text = env.reset()
+            s_text = env_eval.reset()
             s = sent2seq(s_text, seq_len)
             #
             for j in xrange(args.max_ep_steps):
                 # show textual input if so
-                if args.render: env.render()
+                if args.render: env_eval.render()
                 # choose action
                 if np.random.rand() <= 0.05:
-                    a = env.action_space.sample()
+                    a = env_eval.action_space.sample()
                 else:
                     a = model.predictAction(s)
                 # apply action, get rewards and new state s2
-                s2_text, r, terminal, info = env.step(a)
+                s2_text, r, terminal, info = env_eval.step(a)
                 s2 = sent2seq(s2_text, seq_len)
 
                 s = s2
                 ep_reward += r
+                cnt_invalid_actions += 1 if r == -0.1 else 0
 
                 if terminal: break
 
+
             ep_lens.append(j+1)
             invalids.append(cnt_invalid_actions)
-            quests_complete.append(1 if terminal else 0)
+            quests_complete.append(1 if terminal and r>=1 else 0)
+            deaths.append(1 if terminal and r<=0 else 0)
             scores.append(ep_reward)
-        print("> Evaluation {:03d} | len {:.2f} | inval {:.2f} | quests {:.2f} | r {:.2f} ".format(
+        print("> Evaluation {:03d} | len {:.2f} | inval {:.2f} | quests {:.2f} | deaths {:.2f} | r {: .2f} ".format(
             epoch+1, np.mean(ep_lens),
             np.mean(invalids),
             np.mean(quests_complete),
+            np.mean(deaths),
             np.mean(scores)))
         # Save trained model weights and architecture, this will be used by the visualization code
         model.save("model.h5", overwrite=True)
