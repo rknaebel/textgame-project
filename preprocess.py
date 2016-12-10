49697be8db301236e7aea7f231356fefbb6254a0
Rene Knaebel <reneknaebel90@gmail.com>
2016-12-10 16:27:57 +0100

    missed files in last commit

---
+++ preprocess.py
@@ -0,0 +1,11 @@
+
+
+indexer = dict()
+def getIndex(word):
+    if word not in indexer:
+        indexer[word] = len(indexer)+1
+    return indexer[word]
+
+def sent2seq(sentence,length):
+    seq = map(getIndex, text_to_word_sequence(sentence))
+    return seq + [0]*(length-len(seq))
